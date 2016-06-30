ba0ff7f999d2e61bee84b6c5254892473dc250af
Devashish <devashish@mpower-social.com>
2016-06-30 13:43:21 +0600

    Initial Commit

---
+++ app/build/generated/source/buildconfig/release/org/koboc/collect/mpower/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.koboc.collect.mpower;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "org.koboc.collect.mpower";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1026;
+  public static final String VERSION_NAME = "";
+}
